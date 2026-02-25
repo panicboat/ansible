02c469982e1b56ba1710f9df43b9291b526d9315
panicboat <panicboat@gmail.com>
2026-02-25 11:30:16 +0900

    Update git-branch-clean.sh

--- roles/vscode/files/git-branch-clean.sh
+++ roles/vscode/files/git-branch-clean.sh
@@ -8,7 +8,8 @@ for i in "${!list[@]}"
 do
   echo "$i => ${list[$i]}"
   cd ${list[$i]}
-  git switch $(git symbolic-ref refs/remotes/origin/HEAD | cut -f4 -d'/')
+  git checkout -f $(git symbolic-ref refs/remotes/origin/HEAD | cut -f4 -d'/')
+  git clean -fd
   git branch | grep -v "main\|master\|develop\|*" | xargs git branch -D
   git pull origin
 done
